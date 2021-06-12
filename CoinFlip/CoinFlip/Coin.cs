5eaf3b671fdf3f1b26e3393d9be46beb3599f9df
huyhoang-8296 <80611770+huyhoang-8296@users.noreply.github.com>
2021-06-11 23:11:09 -0400

    Add files via upload

---
+++ CoinFlip/CoinFlip/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinFlip
+{
+    class Coin
+    {
+        // Property
+        public bool isHeads { get; set; }
+
+        // constructor for new coin
+        public Coin(bool h)
+        {
+            this.isHeads = h;
+        }
+
+        // Method allows us to flip the coin
+        public void flipCoin(Random r)
+        {
+            // randomly choose heads or tails for our coins 
+            if (r.Next(10) > 5)
+            {
+                this.isHeads = true;
+            }
+            else
+            {
+                this.isHeads = false;
+            }
+        }
+    }
+}
---
+++ CoinFlip/CoinFlip/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinFlip
+{
+    public partial class Form1 : Form
+    {
+
+        // Create an array of coins, list of coins. 5 coins total
+
+        Coin[] coins = new Coin[5];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            // initialized label1 text to be empty 
+            label1.Text = "";
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                label1.Text += coins[i].isHeads + ",";
+                coins[i].flipCoin(r);
+            }
+
+            // Ternery operators. Faster, set images for the coins if heads or tails.
+            pictureBox1.Image = coins[0].isHeads ? Properties.Resources.Coin_head : Properties.Resources.Coin_tail;
+            pictureBox2.Image = coins[1].isHeads ? Properties.Resources.Coin_head : Properties.Resources.Coin_tail;
+            pictureBox3.Image = coins[2].isHeads ? Properties.Resources.Coin_head : Properties.Resources.Coin_tail;
+            pictureBox4.Image = coins[3].isHeads ? Properties.Resources.Coin_head : Properties.Resources.Coin_tail;
+            pictureBox5.Image = coins[4].isHeads ? Properties.Resources.Coin_head : Properties.Resources.Coin_tail;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < coins.Length; i++)
+            {
+                coins[i] = new Coin(false);              
+            }
+        }
+
+    }      
+}
